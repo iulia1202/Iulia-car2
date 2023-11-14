9e9e10b4721f069a8b4aebd70bca5e6cc20ef7fa
Alexia <Alexia@DESKTOP-GQDM62G>
2023-11-14 22:50:44 +0200

    Add project files.

---
+++ Iulia car2/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iulia_car2
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+        public Car(string nameName,string colorName,int yearValue,int maxSpeedValue)
+        {
+            name = nameName;
+            color = colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+        public Car(string nameName)
+        {
+
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+
+    }
+
+}
---
+++ Iulia car2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iulia_car2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Volvo = new Car("XC-90");
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Ford.fullThrottle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.color);
+            Opel.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
